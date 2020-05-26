eacfa61198fe0a029aa9806b26f41074a4342ae4
Jinal <adhyarujinal@gmail.com>
2020-05-26 18:57:47 +0530

    Added login authentication and sessions

--- Novis_Inventis_Backend/app.js
+++ Novis_Inventis_Backend/app.js
@@ -4,6 +4,9 @@ const ejs = require("ejs");
 const truncate = require('truncate-html');
 const _ = require('lodash');
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportlocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -12,7 +15,38 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false    
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/projectTestDB", {useUnifiedTopology: true ,useNewUrlParser: true });
+mongoose.set("useCreateIndex",true);
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String,
+  secret: String
+});
+
+userSchema.plugin(passportlocalMongoose);
+const User = new mongoose.model("User",userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done){
+    done(null,user.id);
+});
+
+passport.deserializeUser(function(id,done){
+    User.findById(id,function(err,user){
+        done(err,user);
+    });
+});
 
 const awardSchema = {
     name: String,
@@ -57,39 +91,59 @@ app.get("/portfolio", function(req,res) {
   });
 });
 
+app.get("/login", function(req,res){
+  res.render("login");
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
+});
+
+
+app.get("/changes", function(req,res){
+   if(req.isAuthenticated()){
+        res.render("changes");
+    }
+    else {
+        res.redirect("/login");
+    }
+});
+
 app.get("/contact", function(req,res) {
     res.render("contact");
 });
 
-  app.get("/compose", function(req,res) {    
-    res.render("compose");
+  app.get("/compose", function(req,res) {  
+    if(req.isAuthenticated()){
+      res.render("compose");
+  }
+  else {
+      res.redirect("/login");
+  }  
   });
 
-  app.post("/composeAward", function(req,res){
-    const award = new Award ({
-      name: req.body.awardName,
-      description: req.body.awardDescription
-    });
-    award.save(function(err){
-      if(!err) {
-        res.redirect("/");
-      }
-    }); 
+  app.get("/update", function(req,res) {    
+    if(req.isAuthenticated()){
+      res.render("update");
+  }
+  else {
+      res.redirect("/login");
+  }
   });
 
-  app.post("/composeCategory", function(req,res){
-    const category = new Category ({
-      name: req.body.categoryName,
-      title: req.body.categoryTitle,
-      content: req.body.categoryContent
-    });
-    category.save(function(err){
-      if(!err) {
-        res.redirect("/");
-      }
-    }); 
+  app.get("/delete", function(req,res) {    
+    if(req.isAuthenticated()){
+      res.render("delete");
+  }
+  else {
+      res.redirect("/login");
+  }
   });
-
 
   app.get("/category/:categoryId", function(req, res){
     const requestedName = _.lowerCase(req.params.categoryName);
@@ -104,6 +158,121 @@ app.get("/contact", function(req,res) {
     });  
   });
 
+ 
+
+  app.post("/register",function(req,res){
+    User.register({username: req.body.username}, req.body.password, function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }
+        else {
+            passport.authenticate("local")(req ,res, function(){
+                res.redirect("/changes");
+            });
+        }
+    });
+});
+
+app.post("/login",function(req,res){
+  const user = new User ({
+      username: req.body.username,
+      password: req.body.password
+  });
+  req.login(user, function(err){
+      if(err){
+          console.log(err);
+      }
+      else {
+          passport.authenticate("local")(req,res,function(){
+            console.log("User found");
+              res.redirect("/changes");
+          });
+          console.log("No user found");
+      }
+  });
+});
+
+
+ app.post("/changes",function(req,res){
+    if(req.isAuthenticated()){
+      res.render("changes");
+  }
+  else {
+      res.redirect("/login");
+  }
+  });
+
+  app.post("/composeAward", function(req,res){
+    const award = new Award ({
+      name: req.body.awardName,
+      description: req.body.awardDescription
+    });
+    award.save(function(err){
+      if(!err) {
+        res.redirect("/changes");
+      }
+    }); 
+  });
+
+  app.post("/composeCategory", function(req,res){
+    const category = new Category ({
+      name: req.body.categoryName,
+      title: req.body.categoryTitle,
+      content: req.body.categoryContent
+    });
+    category.save(function(err){
+      if(!err) {
+        res.redirect("/changes");
+      }
+    }); 
+  });
+
+  app.post("/updateAward", function(req,res){
+    Award.updateOne({name: req.body.awardName}, {description: req.body.awardDescription}, function(err){
+      if(err) {
+        console.log(err);
+      }
+      else {
+        res.redirect("/changes");
+      }
+    })
+  });
+
+  app.post("/updateCategory", function(req,res){
+    Category.updateOne({title: req.body.categoryTitle}, {content: req.body.categoryContent}, function(err){
+      if(err) {
+        console.log(err);
+      }
+      else {
+        res.redirect("/changes");
+      }
+    })
+  });
+
+
+  app.post("/deleteAward", function(req,res) {
+    Award.deleteOne({name: req.body.awardName}, function(err) {
+      if(err) {
+        console.log(err);
+      }
+      else {
+        res.redirect("/changes");
+      }
+    });
+  });
+
+  app.post("/deleteCategory", function(req,res) {
+    Category.deleteOne({title: req.body.categoryTitle}, function(err) {
+      if(err) {
+        console.log(err);
+      }
+      else {
+        res.redirect("/changes");
+      }
+    });
+  });
+  
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });

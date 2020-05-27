a924fef93eb7355361c2149b5d8b69c9baba1298
Jinal <adhyarujinal@gmail.com>
2020-05-27 23:33:35 +0530

    Images and content through database

--- Novis_Inventis_Backend/app.js
+++ Novis_Inventis_Backend/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const path = require('path')
 const truncate = require('truncate-html');
 const _ = require('lodash');
+const crypto = require('crypto');
 const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportlocalMongoose = require("passport-local-mongoose");
+const multer = require('multer')
+const GridFsStorage = require('multer-gridfs-storage')
+const Grid = require('gridfs-stream')
 
 const app = express();
 
@@ -24,6 +29,41 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
+//mongodb URI
+const url = 'mongodb://localhost:27017/imagesDB'
+const conn = mongoose.createConnection(url, ({useUnifiedTopology: true, useNewUrlParser: true}))
+
+//initialize gridfs
+let gfs;
+
+conn.once('open', () => {
+    //initialize stream
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads')
+  })
+
+//Create Storage engine
+const storage = new GridFsStorage({
+  url: url,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        // const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: file.originalname,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+
+const upload = multer({ storage });
+
 
 mongoose.connect("mongodb://localhost:27017/projectTestDB", {useUnifiedTopology: true ,useNewUrlParser: true });
 mongoose.set("useCreateIndex",true);
@@ -71,22 +111,65 @@ app.get("/services", function(req,res) {
     res.render("services");
 });
 
+app.get('/image/:filename' , (req, res) =>{
+  gfs.files.findOne({filename : req.params.filename}, (err, file) =>{
+      // Check if file exists at all
+      if( !file || file.length === 0){
+          return res.status(404).json({
+              err : 'File dosent exists'
+          })
+      }
+      if(file.contentType === 'image/jpeg' || file.contentType === 'image/png'){
+          const readstream = gfs.createReadStream(file.filename);
+          readstream.pipe(res);
+      }else{
+          res.status(404).json({
+              err : 'Not an image....'
+          })
+      }
+  })
+})
+
 app.get("/blog", function(req,res) {
-    Award.find({},function(err,awards) {
-        if(!err) {
-        res.render("blog",{
-            awards: awards
-        });
-        } 
-      });
+  Award.find({},function(err,awards) {
+    if(!err) {
+     gfs.files.find().toArray((err, files) =>{
+      // Check if files exists at all
+      if( !files || files.length === 0){
+        res.render("blog", {files : false,awards: awards})
+      }else{
+          files.map(file =>{
+              if(file.contentType === 'image/jpeg' || file.contentType === 'image/png'){
+                  file.isImage = true;
+              }else{
+                  file.isImage = false;
+              }
+          })
+          res.render("blog", {files : files,awards: awards})
+      }
+  })      
+    }
+  });
 });
 
 app.get("/portfolio", function(req,res) {
   Category.find({},function(err,categories) {
     if(!err) {
-    res.render("portfolio",{
-      categories: categories
-    });
+      gfs.files.find().toArray((err, files) =>{
+        // Check if files exists at all
+        if( !files || files.length === 0){
+          res.render("portfolio", {files : false,categories: categories})
+        }else{
+            files.map(file =>{
+                if(file.contentType === 'image/jpeg' || file.contentType === 'image/png'){
+                    file.isImage = true;
+                }else{
+                    file.isImage = false;
+                }
+            })
+            res.render("portfolio", {files : files,categories: categories})
+        }
+    })      
     } 
   });
 });
@@ -105,9 +188,9 @@ app.get("/logout",function(req,res){
 });
 
 
-app.get("/changes", function(req,res){
+app.get("/changes",upload.single('file'), function(req,res){
    if(req.isAuthenticated()){
-        res.render("changes");
+  res.render("changes");
     }
     else {
         res.redirect("/login");
@@ -147,17 +230,27 @@ app.get("/contact", function(req,res) {
 
   app.get("/category/:categoryId", function(req, res){
     const requestedName = _.lowerCase(req.params.categoryName);
-    const requestedCategoryId = req.params.categoryId;
-  
+    const requestedCategoryId = req.params.categoryId;  
     Category.findOne({_id: requestedCategoryId}, function(err,category){
-      res.render("Category",{
-          name: category.name,
-          title: category.title,
-          content: category.content
-      })
-    });  
+      gfs.files.find().toArray((err, files) =>{
+        // Check if files exists at all
+        if( !files || files.length === 0){
+          res.render("Category", {files : false,name: category.name,
+            title: category.title,
+            content: category.content})
+        }else{
+            files.map(file =>{
+                if(file.contentType === 'image/jpeg' || file.contentType === 'image/png'){
+                    file.isImage = true;
+                }else{
+                    file.isImage = false;
+                }
+            })
+            res.render("Category", {files : files,title: category.title, content: category.content})
+          }
+      })         
+    })    
   });
-
  
 
   app.post("/register",function(req,res){
@@ -185,25 +278,23 @@ app.post("/login",function(req,res){
       }
       else {
           passport.authenticate("local")(req,res,function(){
-            console.log("User found");
               res.redirect("/changes");
           });
-          console.log("No user found");
       }
   });
 });
 
 
- app.post("/changes",function(req,res){
+ app.post("/changes",upload.single('file'),function(req,res){
     if(req.isAuthenticated()){
-      res.render("changes");
+    res.render("changes");
   }
   else {
       res.redirect("/login");
   }
   });
 
-  app.post("/composeAward", function(req,res){
+  app.post("/composeAward", upload.single('file'),function(req,res){
     const award = new Award ({
       name: req.body.awardName,
       description: req.body.awardDescription
@@ -215,7 +306,7 @@ app.post("/login",function(req,res){
     }); 
   });
 
-  app.post("/composeCategory", function(req,res){
+  app.post("/composeCategory",upload.single('file'), function(req,res){
     const category = new Category ({
       name: req.body.categoryName,
       title: req.body.categoryTitle,
@@ -228,7 +319,7 @@ app.post("/login",function(req,res){
     }); 
   });
 
-  app.post("/updateAward", function(req,res){
+  app.post("/updateAward",  upload.single('file'),function(req,res){
     Award.updateOne({name: req.body.awardName}, {description: req.body.awardDescription}, function(err){
       if(err) {
         console.log(err);
@@ -239,7 +330,7 @@ app.post("/login",function(req,res){
     })
   });
 
-  app.post("/updateCategory", function(req,res){
+  app.post("/updateCategory",upload.single('file'), function(req,res){
     Category.updateOne({title: req.body.categoryTitle}, {content: req.body.categoryContent}, function(err){
       if(err) {
         console.log(err);
@@ -252,23 +343,51 @@ app.post("/login",function(req,res){
 
 
   app.post("/deleteAward", function(req,res) {
-    Award.deleteOne({name: req.body.awardName}, function(err) {
+    Award.deleteOne({name: req.body.awardName}, function(err) {    
       if(err) {
         console.log(err);
-      }
+      } 
       else {
-        res.redirect("/changes");
+        const name = req.body.awardName + ".jpg";
+        gfs.files.findOne({filename : name}, (err, file) =>{
+          // Check if file exists at all
+          if( !file || file.length === 0){
+              console.log("There was no image");
+          }
+          else {
+            gfs.remove({ _id: file._id, root: 'uploads' }, (err, gridStore) => {
+              if (err) {
+                console.log(err);
+              }        
+              res.redirect('/changes');
+            });    
+          }
+      })        
       }
     });
   });
 
   app.post("/deleteCategory", function(req,res) {
-    Category.deleteOne({title: req.body.categoryTitle}, function(err) {
+    Category.deleteOne({title: req.body.categoryTitle}, function(err) { 
       if(err) {
         console.log(err);
-      }
+      } 
       else {
-        res.redirect("/changes");
+        const name = req.body.categoryTitle + ".jpg";
+        gfs.files.findOne({filename : name}, (err, file) =>{
+          // Check if file exists at all
+          if( !file || file.length === 0){
+              console.log(err);
+          }
+          else {
+            gfs.remove({ _id: file._id, root: 'uploads' }, (err, gridStore) => {
+              if (err) {
+                console.log(err);
+              }        
+              res.redirect('/changes');
+            });    
+          }
+      })        
       }
     });
   });

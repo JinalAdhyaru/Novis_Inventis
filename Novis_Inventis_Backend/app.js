1df9705f5b3ee41003d47e68e92c08135d8e6d59
Jinal <adhyarujinal@gmail.com>
2020-05-28 12:53:18 +0530

    added image preview

--- Novis_Inventis_Backend/app.js
+++ Novis_Inventis_Backend/app.js
@@ -45,20 +45,15 @@ conn.once('open', () => {
 //Create Storage engine
 const storage = new GridFsStorage({
   url: url,
+  options: {useUnifiedTopology: true},
   file: (req, file) => {
     return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        // const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
+         const fileInfo = {
           filename: file.originalname,
           bucketName: 'uploads'
         };
         resolve(fileInfo);
       });
-    });
   }
 });
 
@@ -188,7 +183,7 @@ app.get("/logout",function(req,res){
 });
 
 
-app.get("/changes",upload.single('file'), function(req,res){
+app.get("/changes", function(req,res){
    if(req.isAuthenticated()){
   res.render("changes");
     }
